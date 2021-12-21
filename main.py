637cfa742ccee548bfafa85d8894cdebf31a268a
draialexis <drai.alexis@gmail.com>
2021-12-21 22:01:49 +0100

    Do linear regression example

--- main.py
+++ main.py
@@ -1,4 +1,76 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+# from six.moves import urllib
+
+# import tensorflow.compat.v2.feature_column as fc
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import tensorflow as tf
 
-print(tf.version)
-print(tf.__version__)
+tf.get_logger().setLevel('FATAL')
+
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+"""
+dftrain.age.hist(bins=20)
+plt.figure()
+dftrain.sex.value_counts().plot(kind='barh')
+plt.figure()
+dftrain['class'].value_counts().plot(kind='barh')
+plt.figure()
+pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
+plt.show()
+"""
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique()
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+
+def make_input_fn(data_df, label_df, num_epochs=10, training=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if training:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+
+    return input_function
+
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, training=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn)
+# result = linear_est.evaluate(eval_input_fn)
+results = list(linear_est.predict(eval_input_fn))
+probs = pd.Series([result['probabilities'][1] for result in results])
+for i in range(5):
+    print(dfeval.loc[i])
+    print('survived?:', y_eval.loc[i])
+    print('estimated chances:', results[i]['probabilities'][1])
+
+# probs.plot(kind='hist', bins=10, title='survival chances')
+# plt.show()
+
+# clear_output()
+# print(result['accuracy'])
