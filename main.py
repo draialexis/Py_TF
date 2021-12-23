21cafd623e5abf97f2d439029a520cbdb68afb3a
draialexis <drai.alexis@gmail.com>
2021-12-23 20:54:25 +0100

    Do classification example

--- main.py
+++ main.py
@@ -1,76 +1,81 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
-# from six.moves import urllib
 
-# import tensorflow.compat.v2.feature_column as fc
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# import os
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf
 
-tf.get_logger().setLevel('FATAL')
+# tf.get_logger().setLevel('FATAL')
 
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
+)
+test_path = tf.keras.utils.get_file(
+    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
+)
 
-"""
-dftrain.age.hist(bins=20)
-plt.figure()
-dftrain.sex.value_counts().plot(kind='barh')
-plt.figure()
-dftrain['class'].value_counts().plot(kind='barh')
-plt.figure()
-pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
-plt.show()
-"""
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
 
-CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
-                       'embark_town', 'alone']
-NUMERIC_COLUMNS = ['age', 'fare']
-
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-    vocabulary = dftrain[feature_name].unique()
-    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
-
-for feature_name in NUMERIC_COLUMNS:
-    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+train_y = train.pop('Species')
+test_y = test.pop('Species')
 
 
-def make_input_fn(data_df, label_df, num_epochs=10, training=True, batch_size=32):
-    def input_function():
-        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
-        if training:
-            ds = ds.shuffle(1000)
-        ds = ds.batch(batch_size).repeat(num_epochs)
-        return ds
-
-    return input_function
+def my_input_fn(features, labels, training=True, batch_size=256):
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+    return dataset.batch(batch_size)
 
 
-train_input_fn = make_input_fn(dftrain, y_train)
-eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, training=False)
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    hidden_units=[30, 10],
+    n_classes=3
+)
 
-linear_est.train(train_input_fn)
-# result = linear_est.evaluate(eval_input_fn)
-results = list(linear_est.predict(eval_input_fn))
-probs = pd.Series([result['probabilities'][1] for result in results])
-for i in range(5):
-    print(dfeval.loc[i])
-    print('survived?:', y_eval.loc[i])
-    print('estimated chances:', results[i]['probabilities'][1])
+classifier.train(
+    input_fn=lambda: my_input_fn(train, train_y),
+    steps=5000
+)
 
-# probs.plot(kind='hist', bins=10, title='survival chances')
-# plt.show()
+eval_result = classifier.evaluate(input_fn=lambda: my_input_fn(test, test_y, training=False))
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+print(eval_result)
 
-# clear_output()
-# print(result['accuracy'])
+
+def input_fn_pred(features, batch_size=256):
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print('enter numbers as prompted')
+for feature in features:
+    valid = False
+    val = "woops"
+    while not valid:
+        val = input(feature + ": ")
+        if val.isdigit():
+            valid = True
+    predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn_pred(predict))
+for pred in predictions:
+    # class_id = pred['class_ids'][0]
+    # probability = pred['probabilities'][class_id]
+    probability = pred['probabilities'][pred['class_ids'][0]]
+
+    # print('\nPRediction is "{}" ({:.1f}%)').format(SPECIES[class_id], 100 * probability)
+    print('\nPrediction is "{}" ({:.1f}%)'.format(SPECIES[pred['class_ids'][0]], 100 * probability))
