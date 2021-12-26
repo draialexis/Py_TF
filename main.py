cc3a56f9658bfb172f0c8fee0ab7a12206fb678c
draialexis <drai.alexis@gmail.com>
2021-12-26 18:31:22 +0100

    Prettify

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import pandas as pd
 
-# import os
-
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf
 
-# tf.get_logger().setLevel('FATAL')
+tf.get_logger().setLevel('FATAL')
 
 CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
-train_path = tf.keras.utils.get_file(
-    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
-)
-test_path = tf.keras.utils.get_file(
-    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
-)
+train_path = tf.keras.utils.get_file("iris_training.csv",
+                                     "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file("iris_test.csv",
+                                    "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
 
 train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
 test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
@@ -58,24 +55,22 @@ def input_fn_pred(features, batch_size=256):
     return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
 
 
-features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
-predict = {}
+my_features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict_dict = {}
 
 print('enter numbers as prompted')
-for feature in features:
+for feature in my_features:
     valid = False
     val = "woops"
     while not valid:
         val = input(feature + ": ")
         if val.isdigit():
             valid = True
-    predict[feature] = [float(val)]
+    predict_dict[feature] = [float(val)]
 
-predictions = classifier.predict(input_fn=lambda: input_fn_pred(predict))
+predictions = classifier.predict(input_fn=lambda: input_fn_pred(predict_dict))
 for pred in predictions:
-    # class_id = pred['class_ids'][0]
-    # probability = pred['probabilities'][class_id]
-    probability = pred['probabilities'][pred['class_ids'][0]]
+    class_id = pred['class_ids'][0]
+    probability = pred['probabilities'][class_id]
 
-    # print('\nPRediction is "{}" ({:.1f}%)').format(SPECIES[class_id], 100 * probability)
-    print('\nPrediction is "{}" ({:.1f}%)'.format(SPECIES[pred['class_ids'][0]], 100 * probability))
+    print('\nPrediction is "{}" ({:.1f}%)'.format(SPECIES[class_id], 100 * probability))
